1654f5c5c1dd1aa24952ca4717b232b07ccde122
Supriya Yatam <supriya.yatam@sidusa.com>
2012-08-10 17:53:58 +0530

    css changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -18,22 +18,16 @@ $(document).ready(function(){
 $('.modules .block-content').hide(); //Hide/close all containers
 $('.modules').find('h3').addClass('modules-trigger');
 //On Click
-$('.modules-trigger').click(function(){   
+$('.modules-trigger').click(function(){
 	if( $(this).next().is(':hidden') ) { //If immediate next container is closed...
-                
 		$('.modules-trigger').removeClass('active').next().slideUp(); //Remove all .acc_trigger classes and slide up the immediate next container
 		$(this).toggleClass('active').next().slideDown(); //Add .acc_trigger class to clicked trigger and slide down the immediate next container
-                
-                return true;
 	}
 	else {
-            
 		$(this).next().slideUp()
 		$('.modules-trigger').removeClass('active').next().slideUp();
-                return true;
 	}
-
-	 //Prevent the browser jump to the link anchor
+	return false; //Prevent the browser jump to the link anchor
 });
 
 });
